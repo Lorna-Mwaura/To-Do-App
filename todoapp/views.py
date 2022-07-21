8a47883c6d483de280aa9fda55c1307c16c49e42
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-07-21 10:55:13 +0300

    created the views

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    todos = TodoList.objects.all()
+    categories = Category.objects.all()
+    if request.method == "POST": 
+        if "taskAdd" in request.POST: 
+            title = request.POST["description"] 
+            date = str(request.POST["date"]) 
+            category = request.POST["category_select"] 
+            content = title + " -- " + date + " " + category 
+            Todo = TodoList(title=title, content=content, due_date=date, category=Category.objects.get(name=category))
+            Todo.save() 
+            return redirect("/")
+        if "taskDelete" in request.POST: 
+            checkedlist = request.POST["checkedbox"] 
+            for todo_id in checkedlist:
+                todo = TodoList.objects.get(id=int(todo_id)) 
+                todo.delete()
+    return render(request, "index.html", {"todos": todos, "categories":categories})
